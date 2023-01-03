537dbc3fa534b7346cba5f2da764b82c7e31a269
John Doe <johndoe@example.com>
2023-01-03 11:34:44 +0100

    new version

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const JourneyTimes = () => {
+  const [response, setResponse] = useState({});
 
-  const onButtonClick = () => {
-    alert('hi')
+  useEffect(() => {
+    const fetchData = async () => {
+      const currentTime = new Date().toISOString();
+      const result = await axios(
+        `https://api.sncf.com/v1/coverage/sncf/journeys?from=stop_area:SNCF:87286526&to=stop_area:SNCF:87286005&datetime=${currentTime}&min_nb_journeys=5`,
+        { auth: ['a27f39bc-3865-4451-8790-01f23ef38bd4', ''] }
+      );
+      console.log(result.data); // log the data returned from the API
+      setResponse(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  const rows = [];
+
+  for (let r = 0; r < 2; r++) {
+    const session = response['journeys'][r]['sections'][1];
+    if ('stop_date_times' in session) {
+      for (const i of session['stop_date_times']) {
+        rows.push({
+          name: i['stop_point']['name'],
+          depart: i['departure_date_time'],
+          arrivee: i['arrival_date_time']
+        });
+      }
+    }
   }
+
   return (
-    <div className="App">
-    <h1>Horaires de train</h1>
-    <button onClick={onButtonClick}>Click me</button>
+    <div>
+      {rows.map(row => (
+        <div key={row.name}>
+          <h3>Name: {row.name}</h3>
+          <p>Depart: {row.depart}</p>
+          <p>Arrivee: {row.arrivee}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
+export default JourneyTimes;
