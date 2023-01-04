272eda165bb273a87acea42d02c78a41491a3a5c
John Doe <johndoe@example.com>
2023-01-04 22:10:25 +0100

    changement dans app.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,8 @@
 import React , { useState } from 'react';
 import JourneyTimes from './JourneyTimes';
 
-function createFormattedDate(dateString) {
-  const year = dateString.substring(0,4);
-  const month = dateString.substring(4,6);
-  const day = dateString.substring(6,8);
-
-  const hours = dateString.substring(9,11);
-  const minutes = dateString.substring(11,13);
-  const seconds = dateString.substring(13,15);
-
-  return new Date(year, month, day, hours, minutes, seconds);
-}
-
-const JourneyTimes = () => {
-  const [response, setResponse] = useState({});
-  useEffect(() => {
-    const fetchData = async () => {
-
-      const currentTime = new Date().toISOString();
-      const result = await fetch(
-        `https://api.sncf.com/v1/coverage/sncf/journeys?from=stop_area:SNCF:87286526&to=stop_area:SNCF:87286005&datetime=${currentTime}&min_nb_journeys=2`,
-        {
-          headers: {
-            Authorization:  process.env.REACT_APP_SNCF_API_KEY,
-          },
-        }
-      );
-      const data = await result.json(); // parse the response as JSON
-      console.log(data); // log the data returned from the API
-      setResponse(data);
-    };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  
-  const rows = [];
-
-  if (response.hasOwnProperty('journeys') && Array.isArray(response.journeys)) {
-    for (const journey of response.journeys) {
-      if (journey.hasOwnProperty('sections') && Array.isArray(journey.sections)) {
-          if (journey.sections[1].hasOwnProperty('type') && journey.sections[1].type === 'public_transport') {
-            if (journey.sections[1].hasOwnProperty('from') && journey.sections[1].from.hasOwnProperty('name')) {
-              if (journey.sections[1].hasOwnProperty('to') && journey.sections[1].to.hasOwnProperty('name') ) {
-                
-                rows.push({
-                  name_depart : journey.sections[1].from.stop_point.name,
-                  name_arrivee : journey.sections[1].to.stop_point.name,
-                  depart: journey.sections[1].departure_date_time,
-                  arrivee: journey.sections[1].arrival_date_time,
-                });
-                
-                //console.log("Hello",journey.sections[1].from.name,journey.sections[1].departure_date_time,journey.sections[1].to.name,journey.sections[1].arrival_date_time,);
-              }
-            }
-          }
-      }
-    }
-  }
-  
-
+function App() {
+  const [selectedValue, setSelectedValue] = useState(60); // curseur
 
   return (
    
